8528640ae995726f63f01e46c5380b9722530eca
neel <neel2292@gmail.com>
2016-05-15 04:32:17 +0800

    Update livebase.js

--- livebase.js
+++ livebase.js
@@ -6,22 +6,28 @@
     "use strict";
     angular.module('livebase')
         //Config
-        .provider('$livebase', function(){
-            var fburl=false;
+        .provider('$livebase', function() {
+            var fburl = false;
+            var commands = [];
             return {
-              url:function(val){
-                  fburl=val;
-              },
-              $get: function () {
-                return {
-                      url:fburl
-                };
-              }
+                url: function(val) {
+                    fburl = val
+                },
+                tell: function(obj) {
+                    commands = obj
+                },
+                $get: function() {
+                    return {
+                        url: fburl,
+                        tell: commands
+                    };
+                }
             };
         })
         //Controllers
         .controller('LivebaseCtrl', ['$scope', 'Bot', function($scope, Bot) {
-            $scope.chats =Bot.all();
+            var rec = new webkitSpeechRecognition();
+            $scope.chats = Bot.all();
             $scope.lb.send = function() {
                 if ($scope.lb.input) {
                     Bot.send($scope.lb.input);
@@ -34,67 +40,119 @@
                     $scope.lb.input = '';
                 }
             }
+
+            $scope.lb.listen = function() {
+                rec.start();
+            }
+
+            rec.onresult = function(event) {
+                Bot.send(event.results[0][0].transcript);
+            }
         }])
 
-        //Directives
-        .directive('livebase', function() {
+    //Directives
+    .directive('livebase', function() {
             return {
                 restrict: 'E',
                 controller: 'LivebaseCtrl',
                 controllerAs: 'lb',
-                transclude:true,
+                transclude: true,
                 template: '<ng-transclude></ng-transclude>'
             }
         })
-        .directive('lbInput', function(){
+        .directive('lbInput', function() {
             return {
-                require:"livebase",
-                restrict:'E',
-                replace:true,
-                template:'<input type="text" ng-model="lb.input" ng-keydown="lb.enter($event)">'
+                require: "livebase",
+                restrict: 'E',
+                replace: true,
+                template: '<input type="text" ng-model="lb.input" ng-keydown="lb.enter($event)">'
             }
         })
-        .directive('lbSend',function(){
-            return{
-                require:'livebase',
-                restrict:'E',
-                replace:true,
-                template:'<button ng-click="lb.send()">Send</button>'
+        .directive('lbSend', function() {
+            return {
+                require: 'livebase',
+                restrict: 'E',
+                replace: true,
+                transclude: true,
+                template: '<button ng-click="lb.send()"><ng-transclude></ng-transclude></button>'
+            }
+        })
+        .directive('lbListen', function() {
+            return {
+                require: 'livebase',
+                restrict: 'E',
+                replace: true,
+                transclude: true,
+                template: '<button ng-click="lb.listen()"><ng-transclude></ng-transclude></button>'
             }
         })
         //Services
-        .service('Bot', ['$http', '$q', '$firebaseArray', '$livebase', function($http, $q, $firebaseArray, $livebase) {
-            var chats=[];
-            if(!localStorage.getItem('livebaseid')){
-                localStorage.setItem('livebaseid', Date.now().toString()+(Math.floor(Math.random() * 100000) + 1));
+        .service('Bot', function($http, $q, $location, $firebaseArray, $livebase) {
+            var chats = [];
+            if (!localStorage.getItem('livebaseid')) {
+                localStorage.setItem('livebaseid', Date.now().toString() + (Math.floor(Math.random() * 100000) + 1));
             }
-            var ref=new Firebase($livebase.url+localStorage.getItem('livebaseid'));
-            chats=$firebaseArray(ref);
+            var ref = new Firebase($livebase.url + localStorage.getItem('livebaseid'));
+            chats = $firebaseArray(ref);
             return ({
                 send: function(text) {
+                    var id;
                     chats.$add({
                         id: Date.now().toString(),
                         text: text,
                         type: 'user'
                     });
-                    var url = "https://samsungiotacademy.com/livebase/gui/plain/bot.php?format=json&submit=Go&say=" + text + localStorage.getItem('livebaseid');
-                    $http.get(url)
-                        .then(function(response) {
-                            var res = response.data.botsay;
-                            for (var script = /<script\b[^>]*>([\s\S]*?)<\/script>/gm, scriptCap = /<Script\b[^>]*>([\s\S]*?)<\/Script>/gm,
-                                    scr; scr = script.exec(res);) res = res.split(scr[0]).join("").trim(), eval(scr[1]);
-                            for (; scr = scriptCap.exec(res);) res = res.split(scr[0]).join("").trim(), eval(scr[1]);
+                    var bypass = matchTell($livebase.tell, text.toLowerCase());
+                    if (bypass) {
+                        if(bypass.reply){
                             chats.$add({
                                 id: Date.now().toString(),
-                                text: res,
+                                text: bypass.reply,
                                 type: 'bot'
                             });
-                        });
+                        }
+                        if(bypass.link){
+                            $location.hash(bypass.link);
+                        }
+                    } else {
+                        if(!localStorage.getItem('livebaseid')){
+                            id=""
+                        }else{
+                            id="&convo_id="+localStorage.getItem('livebaseid')
+                        }
+                        var url = "https://samsungiotacademy.com/livebase/gui/plain/bot.php?format=json&submit=Go&say=" + text + id;
+                        $http.get(url)
+                            .then(function(response) {
+                                var res = response.data.botsay;
+                                for (var script = /<script\b[^>]*>([\s\S]*?)<\/script>/gm, scriptCap = /<Script\b[^>]*>([\s\S]*?)<\/Script>/gm,
+                                        scr; scr = script.exec(res);) res = res.split(scr[0]).join("").trim(), eval(scr[1]);
+                                for (; scr = scriptCap.exec(res);) res = res.split(scr[0]).join("").trim(), eval(scr[1]);
+                                chats.$add({
+                                    id: Date.now().toString(),
+                                    text: res,
+                                    type: 'bot'
+                                });
+                            });
+                    }
                 },
                 all: function() {
                     return chats;
                 }
             })
-        }])
-    ;
+        });
+
+    function matchTell(arr, tell) {
+        if (arr.length === 0){
+            return false;
+        }else{
+            var ret;
+            arr.map(function(i, j, k){
+                if(tell.indexOf(i.cmd.toLowerCase()) > -1){
+                    ret=k[j];
+                }
+            });
+            return ret;
+        }
+    }
+
 })();
